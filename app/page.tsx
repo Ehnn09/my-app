eea4d5eea9673bcb836754e4fb96c8f5f5963b5b
Ehnn09 <nelsoncanonce99@gmail.com>
2024-11-11 02:31:25 +0800

    test

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import '../styles/globals.css';
 const Home = () => (
   <div>
     <h1>Welcome Panget</h1>
-    <Link href="/pages/page1">
+    <Link href="/page1">
       <button>Go to Page 1</button>
     </Link>
   </div>
